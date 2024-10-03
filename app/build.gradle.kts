50fb4994439932ff7072cb33f346c346616af840
jakester0813 <jakester0812@gmail.com>
2024-10-03 12:15:06 -0700

    Made changes to setup the project and change the name of the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.kotlincomposepracticeapp"
-    compileSdk = 34
+    namespace = "com.example.androidkotlincompendium"
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.kotlincomposepracticeapp"
+        applicationId = "com.example.androidkotlincompendium"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -23,6 +23,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -38,6 +39,7 @@ android {
     }
     buildFeatures {
         compose = true
+        dataBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -51,19 +53,62 @@ android {
 
 dependencies {
 
+    //AndroidX
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.biometric)
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    //Fragment
+    implementation(libs.fragment)
+    implementation(libs.fragment.ktx)
+    implementation(libs.fragment.compose)
+
+    //Compose
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
+
+    //Navigation
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+    androidTestImplementation(libs.androidx.navigation.testing)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter)
+
+    implementation(libs.androidx.room)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.rxJava)
+
+    implementation(libs.glide)
+    implementation(libs.picasso)
+    implementation(libs.fresco)
+    implementation(libs.coil)
+
+    implementation(libs.rxJava)
+    implementation(libs.rxAndroid)
+
+    implementation(libs.datastore)
+    implementation(libs.datastore.rxjava3)
+
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    implementation(libs.androidx.work)
+
+    testImplementation(libs.mockito)
+    testImplementation(libs.mockito.kotlin)
+
+    testImplementation(libs.junit)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "KotlinComposePracticeApp"
+rootProject.name = "AndroidKotlinCompendium"
 include(":app")
  
